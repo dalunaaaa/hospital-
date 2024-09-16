8206303eed314a4fa0ecbfc413fefa718cd216d4
Dania <dmluna@scl.edu.gt>
2024-09-16 09:26:30 -0600

    backend listp

---
+++ src/backEnde/BackEnde.java
@@ -0,0 +1,34 @@
+package backEnde;
+
+import hospital.services.BdDoctores;
+import hospital.model.DoctorGeneral;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Optional;
+
+public class BackEnde {
+    public BackEnde(String email) {}
+
+
+    public Map<String, String> validarDatos(String usuario, String contrasena) {
+        Map<String, String> resultado = new HashMap<>();
+        ArrayList<DoctorGeneral> doctores = BdDoctores.ListaDocs();
+
+        Optional<DoctorGeneral> doctorEncontrado = doctores.stream()
+                .filter(doctor -> doctor.getEmail().equals(usuario))
+                .findFirst();
+
+        if (doctorEncontrado.isPresent()) {
+            DoctorGeneral doctor = doctorEncontrado.get();
+            resultado.put("Usuario", doctor.getNombre() + " " + doctor.getApellido());
+            resultado.put("Especialidad", doctor.getEspecialidad());
+            resultado.put("Email", doctor.getEmail());
+        } else {
+            resultado.put("Error", "Usuario no encontrado");
+        }
+
+        return resultado;
+    }
+
+}
--- src/hospital/model/DoctorGeneral.java
+++ src/hospital/model/DoctorGeneral.java
@@ -3,18 +3,32 @@ package hospital.model;
 public class DoctorGeneral {
     private String nombre;
     private String apellido;
-    private String especialidad ;
+    private String especialidad;
     private String email;
-    private String contraseña;
+    private String password;
 
-    public DoctorGeneral (String nombre, String apellido, String especialidad, String email, String contraseña){
+    public DoctorGeneral(String nombre, String apellido, String especialidad, String email, String password) {
         this.nombre = nombre;
         this.apellido = apellido;
         this.especialidad = especialidad;
-        this.email= email;
-        this.contraseña= contraseña;
-
+        this.email = email;
+        this.password = password;
     }
 
+    //SETTER Y GETES
+    public String getNombre() {return nombre;}
+    public void setNombre(String nombre) {this.nombre = nombre;}
+
+    public String getApellido() {return apellido;}
+    public void setApellido(String apellido) {this.apellido = apellido;}
+
+    public String getEspecialidad() {return especialidad;}
+    public void setEmail(String email) {this.email = email;}
+
+    public String getEmail() {return email;}
+    public void setEspecialidad(String especialidad) {this.especialidad = especialidad;}
+
+    public String getPassword() {return password;}
+    public void setPassword(String password) {this.password = password;}
 }
 
