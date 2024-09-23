dfcf9d803179ab19db6069eedf8c14df3cf480a1
Dania <dmluna@scl.edu.gt>
2024-09-23 08:49:54 -0600

    primera version menu lateral

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,9 @@
 import hospital.controller.LoginController;
 import hospital.view.LoginView;
-import backEnde.BackEnde;
-import javax.swing.*;
 
-public class Main extends JFrame {
-
+public class Main {
     public static void main(String[] args) {
-
         LoginView loginView = new LoginView();
-        LoginController loginController  = new LoginController(loginView);
-        BackEnde backEnde = new BackEnde(loginView.getEmail());
-
+        new LoginController(loginView);
     }
 }
--- src/hospital/controller/LoginController.java
+++ src/hospital/controller/LoginController.java
@@ -1,6 +1,9 @@
 package hospital.controller;
 
+import hospital.view.DoctorView;
 import hospital.view.LoginView;
+
+import java.util.HashMap;
 
 public class LoginController {
     private LoginView loginView;
@@ -8,13 +11,29 @@ public class LoginController {
     public LoginController(LoginView loginView) {
         this.loginView = loginView;
 
-        this.loginView.addLoginListener(e -> {
-            iniciarSesion();
-        });
+        this.loginView.addLoginListener(e -> iniciarSesion());
     }
-
 
     public void iniciarSesion() {
-        System.out.println("Hola mundo");
+        String email = loginView.getEmail();
+        String password = loginView.getPassword();
+
+
+        abrirDoctorView();
+    }
+
+    private void abrirDoctorView() {
+
+        loginView.dispose();
+
+
+        HashMap<String, String> datosDoctor = new HashMap<>();
+        datosDoctor.put("nombre", "Dania Luna");
+        datosDoctor.put("especialidad", "Doctor General");
+
+
+        DoctorView doctorView = new DoctorView(datosDoctor);
+        doctorView.setVisible(true);
     }
 }
+
